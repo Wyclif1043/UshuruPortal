fbbc46955a4c1fe2d0c40b17a8afc60bd64db915
Wyclif1043 <wyclif.omondi@swizzsoft.com>
2025-11-26 04:04:59 -0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: ["1431392487db.ngrok-free.app"] // or "*.ngrok-free.app"
-  }
 })
